ca66f223c22a4ac9b6c460900337339deee5d134
white-richard <rich.white285@yahoo.com>
2026-05-15 20:02:11 -0700

    add bayes from pa2; finish bayes for pa4

--- pa_4/src/main.py
+++ pa_4/src/main.py
@@ -2,7 +2,9 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 
-# Note: sklearn.svm uses LibSVM in its backend
+from bayes import est_sample_cov, est_sample_mean, mahalanobis_distance
+
+# Note: sklearn.svm uses LibSVM in its backend so I can use python :)
 
 # All of our params
 resolutions = ["16_20", "48_60"]
@@ -16,6 +18,7 @@ for resolution in resolutions:
     print(f"Resolution: {resolution}")
     poly_test_errors = []
     rbf_test_errors = []
+    bayes_test_errors = []
 
     for fold in folds:
         x_train = np.loadtxt(f"data/{resolution}/trPCA_{fold:02d}.txt")[:, :n_eigens]
@@ -64,6 +67,19 @@ for resolution in resolutions:
         rbf_test_err = (clf.predict(x_test_s) != y_test).mean() * 100
         rbf_test_errors.append(rbf_test_err)
 
+        # ML estimation
+        classes = np.unique(y_train)
+        means, covs = {}, {}
+        for c in classes:
+            x_c = x_train[y_train == c]
+            means[c] = est_sample_mean(x_c)
+            covs[c] = np.diag(np.diag(est_sample_cov(x_c, means[c])))
+
+        d = {c: mahalanobis_distance(x=x_test, mean=means[c], cov=covs[c]) for c in classes}
+        bayes_preds = np.where(d[classes[0]] < d[classes[1]], classes[0], classes[1])
+        bayes_test_err = (bayes_preds != y_test).mean() * 100
+        bayes_test_errors.append(bayes_test_err)
+
         print(f"Fold: {fold}:")
         print(
             f"Poly best: C={best_poly_params[0]}, D={best_poly_params[1]} | val err={best_poly_err:.2f}% | test err={poly_test_err:.2f}%",
@@ -71,6 +87,8 @@ for resolution in resolutions:
         print(
             f"RBF best: C={best_rbf_params[0]}, gamma={best_rbf_params[1]} | val err={best_rbf_err:.2f}% | test err={rbf_test_err:.2f}%",
         )
+        print(f"Bayes | test err={bayes_test_err:.2f}%")
 
     print(f"{resolution} Poly avg test error: {np.mean(poly_test_errors):.2f}%")
     print(f"{resolution} RBF avg test error: {np.mean(rbf_test_errors):.2f}%")
+    print(f"{resolution} Bayes avg test error: {np.mean(bayes_test_errors):.2f}%")
